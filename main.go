07e3d91bd648bccf1e7e18a417561a9496563d9d
Shreyas Bhat <sbhat@fnal.gov>
2025-05-09 12:07:30 -0500

    Updated call to gfal2Client.getFilesTree to work with new method signature

--- main.go
+++ main.go
@@ -64,7 +64,7 @@ func main() {
 	}
 
 	source := "https://fndcadoor.fnal.gov:2880/" + exptArea + "/resilient/jobsub_stage/"
-	files, err := client.getFilesTree(ctx, source, nil)
+	files, err := client.getFilesTree(ctx, source, nil, nil)
 	switch {
 	case errors.Is(err, errFileCountLimitExceeded):
 		fmt.Println("file count limit exceeded. Stopping collecting files now")

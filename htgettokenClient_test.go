0bf642afd1ba79cfe760080aa0dde25c2066fb85
Shreyas Bhat <sbhat@fnal.gov>
2025-05-28 11:11:21 -0500

    prepareHtgettokenopts returns a []string

--- htgettokenClient_test.go
+++ htgettokenClient_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"log/slog"
+	"os"
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,55 +20,60 @@ func TestPrepareHtgettokenopts(t *testing.T) {
 	type testCase struct {
 		description string
 		options     []string
-		expected    string
+		expected    []string
 	}
 
 	testCases := []testCase{
 		{
+			"Empty string",
+			[]string{""},
+			[]string{},
+		},
+		{
 			"Option with value",
 			[]string{"--option=value"},
-			"--option=value",
+			[]string{"--option=value"},
 		},
 		{
 			"Option without value",
 			[]string{"--option"},
-			"--option",
+			[]string{"--option"},
 		},
 		{
 			"Single-dashed option without value",
 			[]string{"-o"},
-			"-o",
+			[]string{"-o"},
 		},
 		{
 			"Option with space and value",
 			[]string{"--option", "value"},
-			"--option=value",
+			[]string{"--option=value"},
 		},
 		{
 			"Single-dashed option with value",
 			[]string{"-o", "value"},
-			"-o=value",
+			[]string{"-o=value"},
 		},
 		// These are malformed, but we should handle them
 		{
 			"Option with space and value",
 			[]string{"--option value"},
-			"--option=value",
+			[]string{"--option=value"},
 		},
 		{
 			"Single-dashed option with value",
 			[]string{"-o value"},
-			"-o=value",
+			[]string{"-o=value"},
 		},
 		{
 			"Extra spaces",
 			[]string{"--option  value ", "--option2=value2"},
-			"--option=value --option2=value2",
+			[]string{"--option=value", "--option2=value2"},
 		},
 		{
 			"Mix of options with and without values, with malformed options mixed in",
 			[]string{"--option=value", "--option2=value2", "--option3", "value3", "--option4  value4 "},
-			"--option=value --option2=value2 --option3=value3 --option4=value4",
+			[]string{"--option=value", "--option2=value2", "--option3=value3", "--option4=value4"},
 		},
 	}
 

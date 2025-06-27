20c7e3d595b04cc99e4c11fc70fcec60a0f8e713
Shreyas Bhat <sbhat@fnal.gov>
2025-06-27 11:02:19 -0500

    Upgraded htcondor-go to 1.4.1, use classad.MapStringStringToClassAd in tests

--- condorSchedd_test.go
+++ condorSchedd_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/retzkek/htcondor-go/classad"
@@ -18,25 +19,25 @@ func TestCondorScheddGetDropboxFilesFromJob(t *testing.T) {
 	testCases := []testCase{
 		{
 			"One file",
-			mapStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile"}),
+			classad.MapStringStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile"}),
 			[]string{"/path/to/myfile"},
 			nil,
 		},
 		{
 			"Two files",
-			mapStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile,/path/to/myfile2"}),
+			classad.MapStringStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile,/path/to/myfile2"}),
 			[]string{"/path/to/myfile", "/path/to/myfile2"},
 			nil,
 		},
 		{
 			"Three files, comma-space",
-			mapStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile,/path/to/myfile2, /path/to/myfile3"}),
+			classad.MapStringStringToClassAd(map[string]string{"PNFS_INPUT_FILES": "/path/to/myfile,/path/to/myfile2, /path/to/myfile3"}),
 			[]string{"/path/to/myfile", "/path/to/myfile2", "/path/to/myfile3"},
 			nil,
 		},
 		{
 			"Missing key in job",
-			mapStringToClassAd(map[string]string{"PNFS_INPUT_FILES_WRONG": "/path/to/myfile,/path/to/myfile2, /path/to/myfile3"}),
+			classad.MapStringStringToClassAd(map[string]string{"PNFS_INPUT_FILES_WRONG": "/path/to/myfile,/path/to/myfile2, /path/to/myfile3"}),
 			nil,
 			errMissingJobDropboxFiles,
 		},
@@ -92,10 +93,61 @@ func TestBuildConstraint(t *testing.T) {
 	}
 }
 
-func mapStringToClassAd(m map[string]string) classad.ClassAd {
-	ad := make(classad.ClassAd)
-	for k, v := range m {
-		ad[k] = classad.AttributeFromString(v)
+// TODO Use Testcontainers for this?
+func TestGetCondorSchedds(t *testing.T) {}
+
+func TestSetupIDTOKENEnvironment(t *testing.T) {
+	type testCase struct {
+		description  string
+		oldValue     string
+		cleanupCheck func() bool
 	}
-	return ad
+
+	testCases := []testCase{
+		{
+			"_condor_SEC_CLIENT_AUTHENTICATION_METHODS unset",
+			"",
+			func() bool {
+				_, ok := os.LookupEnv("_condor_SEC_CLIENT_AUTHENTICATION_METHODS")
+				return !ok
+			},
+		},
+		{
+			"_condor_SEC_CLIENT_AUTHENTICATION_METHODS set",
+			"some_value",
+			func() bool {
+				val, ok := os.LookupEnv("_condor_SEC_CLIENT_AUTHENTICATION_METHODS")
+				return ok && val == "some_value"
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(
+			test.description,
+			func(t *testing.T) {
+				// Set the environment variable to an empty string
+				t.Setenv("_condor_SEC_CLIENT_AUTHENTICATION_METHODS", test.oldValue)
+
+				// Call the function to set up the environment
+				cleanup := setupIDTOKENEnvironment()
+
+				// Check if the environment variable is set correctly
+				value, ok := os.LookupEnv("_condor_SEC_CLIENT_AUTHENTICATION_METHODS")
+				if !ok {
+					t.Errorf("Environment variable _condor_SEC_CLIENT_AUTHENTICATION_METHODS should be set")
+				}
+				// Assert that the environment variable is set to "IDTOKENS"
+				if value != "IDTOKENS" {
+					t.Errorf("Expected _condor_SEC_CLIENT_AUTHENTICATION_METHODS to be 'IDTOKENS', got '%s'", value)
+				}
+				// Call the cleanup function
+				cleanup()
+				if !test.cleanupCheck() {
+					t.Errorf("Cleanup function did not restore the environment correctly")
+				}
+			},
+		)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/knadh/koanf/providers/posflag v1.0.1
 	github.com/knadh/koanf/v2 v2.2.1
 	github.com/lestrrat-go/jwx v1.2.7
-	github.com/retzkek/htcondor-go v1.4.0
+	github.com/retzkek/htcondor-go v1.4.1
 	github.com/scitokens/scitokens-go v0.3.2
 	github.com/spf13/pflag v1.0.6
 	github.com/stretchr/testify v1.10.0
@@ -24,8 +24,8 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.7.8 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/knadh/koanf/maps v0.1.2 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
@@ -37,16 +37,16 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.20.5 // indirect
-	github.com/prometheus/client_model v0.6.1 // indirect
-	github.com/prometheus/common v0.55.0 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/prometheus/client_golang v1.22.0 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.63.0 // indirect
+	github.com/prometheus/procfs v0.16.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/otel v1.34.0 // indirect
-	go.opentelemetry.io/otel/metric v1.34.0 // indirect
-	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

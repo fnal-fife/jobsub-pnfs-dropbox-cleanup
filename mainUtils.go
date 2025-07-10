b793ec7a74279bbd1769bc657a499e89e9dfa4eb
Shreyas Bhat <sbhat@fnal.gov>
2025-07-10 15:16:03 -0500

    Broke out functional logger for main and run, and moved vault token checking to own func

--- mainUtils.go
+++ mainUtils.go
@@ -2,34 +2,15 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io/fs"
 	"log/slog"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
-
-func stripPNFSFromPath(pnfsPath string) string {
-	return strings.TrimPrefix(pnfsPath, "/pnfs")
-}
-
-func PNFSToHTTPS(pnfsPath string, urlHostPort string, filenameTransformFunc func(string) string) string {
-	u, err := url.Parse(urlHostPort)
-	if err != nil {
-		slog.Error("error parsing URL", "error", err)
-		return ""
-	}
-	return u.JoinPath(filenameTransformFunc(pnfsPath)).String()
-}
-
-func userConfigDir() string {
-	dir, err := os.UserConfigDir()
-	if err != nil {
-		slog.Error("error getting user config directory", "error", err)
-		return ""
-	}
-	return dir
-}
 
 func getConfigFilePath(configFileFlagVal string, checkDirs []string) (string, error) {
 	// Check configFileFlag first
@@ -54,6 +35,55 @@ func getConfigFilePath(configFileFlagVal string, checkDirs []string) (string, er
 	return "", errNoConfigFileFound
 }
 
+func checkVaultTokenFile(location, ageCutoff string) error {
+	// vaultTokenAgeCutoff, err := time.ParseDuration(k.String("vault.vaultTokenAgeCutoff"))
+	funcLogger := logger.With("caller", "checkVaultTokenFile")
+	vaultTokenAgeCutoff, err := time.ParseDuration(ageCutoff)
+	if err != nil {
+		funcLogger.Error("error parsing vault token age cutoff duration. Using default vault token age cutoff", "error", err, "defaultVaultTokenAgeCutoff", defaultVaultTokenAgeCutoff)
+		vaultTokenAgeCutoff = defaultVaultTokenAgeCutoff
+	}
+
+	// Is vault token file new enough?
+	// runLogger.Debug("Ensuring vault token is available, and new enough", "vaultTokenFile", k.String("vault.vaultTokenFile"), "vaultTokenAgeCutoff", vaultTokenAgeCutoff)
+	funcLogger.Debug("Ensuring vault token is available, and new enough", "vaultTokenFile", location, "vaultTokenAgeCutoff", ageCutoff)
+	stat, err := os.Stat(location)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return errNoVaultTokenFile
+		}
+		return err
+	}
+	if stat.ModTime().Add(vaultTokenAgeCutoff).Before(now) { // File is older than vaultTokenAgeCutoff
+		return errVaultTokenTooOld
+	}
+	return nil
+}
+
+func stripPNFSFromPath(pnfsPath string) string {
+	return strings.TrimPrefix(pnfsPath, "/pnfs")
+}
+
+func PNFSToHTTPS(pnfsPath string, urlHostPort string, filenameTransformFunc func(string) string) string {
+	u, err := url.Parse(urlHostPort)
+	if err != nil {
+		slog.Error("error parsing URL", "error", err)
+		return ""
+	}
+	return u.JoinPath(filenameTransformFunc(pnfsPath)).String()
+}
+
+func userConfigDir() string {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		slog.Error("error getting user config directory", "error", err)
+		return ""
+	}
+	return dir
+}
+
 var (
 	errNoConfigFileFound = errors.New("no config file found in any of the specified directories")
+	errNoVaultTokenFile  = fmt.Errorf("could not find vault token file at configured location: %w", fs.ErrNotExist)
+	errVaultTokenTooOld  = errors.New("vault token file is older than the time cutoff")
 )

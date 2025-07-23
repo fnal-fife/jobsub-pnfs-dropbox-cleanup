14530fc6346c2da53643474f6291aa16fd5f8955
Shreyas Bhat <sbhat@fnal.gov>
2025-07-23 16:36:27 -0500

    Removed testing dependency and replaced t.Log/t.Error statements with slog statements

--- internal/testserver/server.go
+++ internal/testserver/server.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
-	"testing"
 	"time"
 )
 
@@ -13,11 +13,12 @@ import (
 // The server has a readiness endpoint at /ready and the following test endpoints:
 //
 //	/testexperiment/resilient/jobsub_stage/file1
+//	/testexperiment/resilient/jobsub_stage/dir1
 //
 // To shut down the server, the caller should cancel the input context ctx. StartServer
 // returns a channel, shutdown, that is closed when the server is shut down,
 // and an error if the server did not start up correctly.
-func StartServer(ctx context.Context, t *testing.T) (shutdown chan struct{}, startupErr error) {
+func StartServer(ctx context.Context) (shutdown chan struct{}, startupErr error) {
 	shutdown = make(chan struct{}) // Channel to signal to the caller that the server is shutdown
 	server := &http.Server{
 		Addr: ":8080",
@@ -28,37 +29,38 @@ func StartServer(ctx context.Context, t *testing.T) (shutdown chan struct{}, sta
 
 	// Handlers
 	mux.HandleFunc("/ready", handleReady)
-	mux.HandleFunc("/testexperiment/resilient/jobsub_stage/file1", handleFile1Func(t))
-	mux.HandleFunc("/testexperiment/resilient/jobsub_stage/dir1", handleDir1Func(t))
+	mux.HandleFunc("/testexperiment/resilient/jobsub_stage/file1", handleFile1)
+	mux.HandleFunc("/testexperiment/resilient/jobsub_stage/dir1", handleDir1)
 
 	// Start the server in a goroutine
 	go func() {
 		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			t.Errorf("Server failed: %v", err)
+			slog.Error("Server failed", "error", err)
 		}
+		slog.Info("Server closed")
 		close(shutdown)
 	}()
 
 	// Wait for the server to be ready
 	ready := false
 	for i := 0; i < 10; i++ {
-		t.Logf("try %d of 10 to check if server is ready", i+1)
+		slog.Info("checking if server is ready", "try", i+1, "totalTries", 10)
 		func() {
 			defer time.Sleep(500 * time.Millisecond)
 			r, err := http.Get("http://localhost:8080/ready")
 			if err != nil {
-				t.Logf("Failed to send GET request: %v", err)
+				slog.Error("Failed to send GET request", "error", err)
 				return
 			}
 			defer r.Body.Close()
 			if r.StatusCode != http.StatusOK {
-				t.Logf("Expected status OK, got %s", r.Status)
+				slog.Error("Unexpected response status", "expected", http.StatusOK, "got", r.Status)
 				return
 			}
 			ready = true
 		}()
 		if ready {
-			t.Log("Server is ready")
+			slog.Info("Server is ready")
 			break
 		}
 	}
@@ -67,7 +69,7 @@ func StartServer(ctx context.Context, t *testing.T) (shutdown chan struct{}, sta
 		c, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancelShutdown()
 		if err := server.Shutdown(c); err != nil {
-			t.Errorf("Server shutdown failed: %v", err)
+			slog.Error("Server shutdown failed", "error", err)
 		}
 		return nil, errors.New("server did not become ready after 10 retries")
 	}
@@ -76,7 +78,7 @@ func StartServer(ctx context.Context, t *testing.T) (shutdown chan struct{}, sta
 	go func() {
 		<-ctx.Done()
 		if err := server.Shutdown(context.Background()); err != nil {
-			t.Errorf("Server shutdown failed: %v", err)
+			slog.Error("Server shutdown failed", "error", err)
 		}
 	}()
 
@@ -87,24 +89,18 @@ func handleReady(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "ready")
 }
 
-func handleFile1Func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+func handleFile1(w http.ResponseWriter, r *http.Request) {
 	// /testexperiment/resilient/jobsub_stage/file1
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodDelete {
-			t.Log("Received DELETE request for /testexperiment/resilient/jobsub_stage/file1")
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+	if r.Method == http.MethodDelete {
+		slog.Info("Received DELETE request for /testexperiment/resilient/jobsub_stage/file1")
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
-func handleDir1Func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+func handleDir1(w http.ResponseWriter, r *http.Request) {
 	// /testexperiment/resilient/jobsub_stage/dir1
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodDelete {
-			t.Log("Received DELETE request for /testexperiment/resilient/jobsub_stage/dir1")
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+	if r.Method == http.MethodDelete {
+		slog.Info("Received DELETE request for /testexperiment/resilient/jobsub_stage/dir1")
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
